780b024c6e6d04732108b61faf0995d89d4d4311
Archit Jain <architjain2005@gmail.com>
2023-11-20 00:56:47 +0530

    12

--- Timepass/app/src/main/java/com/example/timepass/MainActivity.java
+++ Timepass/app/src/main/java/com/example/timepass/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.timepass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1=findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Hello world",Toast.LENGTH_LONG).show();
+                Intent intent= new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
